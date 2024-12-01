c76bc5fce92cace917b4f6991f9990cde52ac87a
pasindu gayan <pasindu9225@gmail.com>
2024-12-01 17:01:50 +0530

    create user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,82 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+    avatar: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    },
+    mobile: {
+      type: String,
+      default: null,
+    },
+    refresh_token: {
+      type: String,
+      default: "",
+    },
+    verify_email: {
+      type: Boolean,
+      default: false,
+    },
+    last_login: {
+      type: Date,
+      default: "",
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive", "suspended"],
+      default: "active",
+    },
+    address_details: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "address",
+      },
+    ],
+    shopping_cart: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "cartProduct",
+      },
+    ],
+    orderHistory: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Order",
+      },
+    ],
+    forgot_password_otp: {
+      type: String,
+      default: null,
+    },
+    forgot_password_expiry: {
+      type: Date,
+      default: null,
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
